97b31a6a41c7e1465185e3299c7a6677d4987c92
user-name014 <tatsuya02room@gmail.com>
2024-04-17 15:39:54 +0900

    2nd commit

---
+++ src/app/components/AppBar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="text-xl font-bold p-4 shadow-md">
+      <Link href="/">Header</Link>
+    </div>
+  );
+};
+
+export default Header;
